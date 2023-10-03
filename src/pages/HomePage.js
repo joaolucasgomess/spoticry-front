2ee8e03c2446b61d9da86a823c5529f7534aa4e0
FlavioAAMotta <flavio.motta@sga.pucminas.br>
2023-10-03 15:53:56 -0300

    Footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import { Header } from "../components/Header/Header";
 import { Banner } from "../components/Banner/Banner";
 import { Main } from "../components/Main/Main";
+import { Footer } from "../components/Footer/Footer";
 
 export const HomePage = () => {
 
@@ -9,6 +10,7 @@ export const HomePage = () => {
       <Header />
       <Banner />
       <Main />
+      <Footer / >
     </div>
   );
 };
