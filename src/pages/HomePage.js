209a41f68133cd570886dcda1c3963297a1d14c3
FlavioAAMotta <flavio.motta@sga.pucminas.br>
2023-10-03 14:59:55 -0300

    Banner

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { goToLoginPage } from "../routes/Coordinator";
 import { Header } from "../components/Header/Header";
+import { Banner } from "../components/Banner/Banner";
 
 export const HomePage = () => {
   const navigate = useNavigate();
@@ -13,6 +14,7 @@ export const HomePage = () => {
   return (
     <div>
       <Header />
+      <Banner />
       <h1>Home</h1>
       <p>home página</p>
       <button onClick={login}>Login</button>
