67698c1e46db50a1015df991eca3af7d577e4e8d
JOAO LUCAS GOMES NAPOLES <1-22-11755@aluno.faminas.edu.br>
2023-11-17 19:10:47 -0300

    Loading na tela de detalhes da playlist

--- src/components/MainDetailPlaylist/MainDetailPlaylist.jsx
+++ src/components/MainDetailPlaylist/MainDetailPlaylist.jsx
@@ -1,51 +1,56 @@
 import { urlImage } from '../../constants/urls'
 import { ContainerTitle, ContainerNavBar } from './Styles'
 import { ListSong } from '../ListSong/ListSong'
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Loading } from "../Loading/Loading";
 import { getPlaylistById } from "../../services/playlist"
 
 export const MainDetailPlaylist = (props) => {
     const [playlist, setPlaylist] = useState({})
-    const [isLoading, setLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetchPlaylistsById()
     }, []);
-    
+
     const fetchPlaylistsById = async () => {
         try {
             const response = await getPlaylistById(props.playlistId.playlistId)
             setPlaylist(response.data.playlist);
-            setLoading(false)
+            setIsLoading(false)
         } catch (error) {
-          console.error("Erro ao buscar playlists:", error)
+            console.error("Erro ao buscar playlists:", error)
         }
     }
 
-    return(
-        <>  
-            <ContainerTitle>
-                <div>
-                    <img src={urlImage} alt="icon-playlist" />
-                </div>
-                <div>
-                    <p>Playlist</p>
-                    {/* <h1>My Playlist</h1> */}
-                    <h1>{playlist._name}</h1>
-                </div>
-            </ContainerTitle>
-            <ContainerNavBar>
-                <div>
-                    <button>Player</button>
-                    <button>Download</button>
-                    <button>Mais</button>
-                </div>
-                <div>
-                    <input type="text" placeholder="Pesquisa" />
-                </div>
-            </ContainerNavBar>
-            <ListSong/>
+    return (
+        <>
+            {!props.playlistId.playlistId ? setIsLoading(false) : !isLoading ?
+                <>
+                    <ContainerTitle>
+                        <div>
+                            <img src={urlImage} alt="icon-playlist" />
+                        </div>
+                        <div>
+                            <p>Playlist</p>
+                            {!props.playlistId.playlistId? 
+                                <h1>My Playlist</h1>
+                            :   <h1>{playlist._name}</h1>}
+                        </div>
+                    </ContainerTitle>
+                    <ContainerNavBar>
+                        <div>
+                            <button>Player</button>
+                            <button>Download</button>
+                            <button>Mais</button>
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Pesquisa" />
+                        </div>
+                    </ContainerNavBar>
+                    <ListSong />
+                </>
+            : <Loading/>}
         </>
     )
 }
